2bbf27b42b6c02c61ef4b4f90ba40f2e928be3e3
Recep KOÇAĞA <kocagarecep@gmail.com>
2023-06-17 11:36:29 +0300

    @model Direktifi

--- Basics/Controllers/EmployeeController.cs
+++ Basics/Controllers/EmployeeController.cs
@@ -1,20 +1,35 @@
+using Basics.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basics.Controllers
 {
     public class EmployeeController : Controller
     {
-        public String Index(){
-            return "hello";
+        public IActionResult Index1(){
+            string message = $"HELLO WORLD. {DateTime.Now.ToString()}";
+            return View("Index1",message);
         }
 
         public ViewResult Index2()  //ViewResult: görünüm sonucu
         {
-           return View("Index"); //view altındaki employee klasörü altındaki Index çalıştır
+            var names = new String[]
+            {
+              "Recep",
+              "Turgay",
+              "Merve"
+            };
+           return View("Index2",names); //view altındaki employee klasörü altındaki Index çalıştır
         }
 
         public IActionResult Index3(){
-            return Content("Employee");
+            var list = new List<Employee>()
+            {
+               new Employee(){Id=1, FirstName="Recep", LastName="koçağa", Age=29},
+               new Employee(){Id=2, FirstName="turgay", LastName="türker", Age=25},
+               new Employee(){Id=3, FirstName="deniz", LastName="sever", Age=21},
+
+            };
+            return View("Index3",list);
         }
     }
     

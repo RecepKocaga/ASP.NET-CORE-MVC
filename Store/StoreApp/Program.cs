94246b2d88d84068ba047c4e6b812a653ba311ad
Recep KOÇAĞA <kocagarecep@gmail.com>
2023-06-19 11:26:08 +0300

    Mvc project template

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,7 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+//web uygulamasına Services (builder)inşaet ya da ekle
+//builder inşa et
+//Build derleme
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/btk", () => "BTK Akademi!");
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

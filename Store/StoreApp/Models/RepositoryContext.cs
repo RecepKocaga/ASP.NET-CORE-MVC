3cfa6665bb9388e291437aaf7b60f3bc9debb895
Recep KOÇAĞA <kocagarecep@gmail.com>
2023-06-19 13:09:44 +0300

    package management and Creating RepositoryContext.cs

---
+++ Store/StoreApp/Models/RepositoryContext.cs
@@ -0,0 +1,10 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApp.Models
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } //Product modelinde olan alanları Products tablosuna at
+    }
+}
